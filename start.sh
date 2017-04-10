66b914d273cd6eee664a227b7406b0fa886c6eb3
gwtlewis <gwtlewis@qq.com>
2017-04-10 23:57:56 +0800

    enhance start script

--- start.sh
+++ start.sh
@@ -4,4 +4,10 @@ if [ ! -d "./logs" ]; then
   mkdir ./logs
 fi
 
+pid=$(ps -ef | grep "REST" | grep -v grep | awk '{print $2}')
+
+if [ -n "$pid" ]; then
+    kill ${pid}
+fi
+
 nohup python ./REST.py 8888 >> ./logs/console.log &
